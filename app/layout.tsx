89ce62f3da2b9d489e3b43d763395246fa6726bf
sparkycake0 <ogirajko02@gmail.com>
2025-02-01 19:40:46 +0000

    release1

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,26 +2,28 @@ import { Nunito_Sans } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import { LayoutProps } from "@/.next/types/app/layout";
-import { Providers } from "./chakra/providers";
 import UserCheck from "./lib/userchecking";
 
-const font = Nunito_Sans({ subsets: ["latin"] });
+const font = Nunito_Sans({
+  subsets: ["latin"],
+  variable: "--font-nunito-sans",
+});
 export const metadata = {
   title: "Xenity - Chat App",
-  description: "Created in order to get you friends. Made by WeCanHackNasa.",
+  description: "Created in order to get you friends. Made by LunarWeb.",
 };
 
 export default function RootLayout({ children }: LayoutProps) {
   return (
     <html lang="en">
       <body
-        className={`${font.className} flex flex-col lg:flex-row h-screen bg-neutral-900 text-white overflow-x-hidden`}
+        className={`${font.variable} flex flex-col lg:flex-row h-screen bg-neutral-900 text-white overflow-x-hidden`}
       >
         <Navbar />
 
         <div className="flex flex-col flex-grow lg:ml-32 mt-10 lg:mt-0 ">
           {children}
-          <UserCheck />
+          <UserCheck children={children} />
         </div>
       </body>
     </html>
